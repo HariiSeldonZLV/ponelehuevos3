badbaec5d2a47ac9b91a7d7a6a8cb901ea62a2ab
Denis Salinas <dje.salinas.m@gmail.com>
2026-03-30 12:35:11 -0300

    fix: agregar archivos de environment con valores vacíos

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB7xEyz5VcMsoUTCpqwcLJYA16WZaWBsa4",
+    authDomain: "ponelehuevos.firebaseapp.com",
+    projectId: "ponelehuevos",
+    storageBucket: "ponelehuevos.firebasestorage.app",
+    messagingSenderId: "703920721116",
+    appId: "1:703920721116:web:f5444132ad80346a577819"
+  }
+};
