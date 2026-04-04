00a69e2906c0123951c1f22148270277eedbef1c
Denis Salinas Morales <dje.salinas.m@gmail.com>
2026-04-04 13:58:49 -0300

    Clear Firebase configuration in production environment
    Removed sensitive Firebase configuration values.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB7xEyz5VcMsoUTCpqwcLJYA16WZaWBsa4",
-    authDomain: "ponelehuevos.firebaseapp.com",
-    projectId: "ponelehuevos",
-    storageBucket: "ponelehuevos.firebasestorage.app",
-    messagingSenderId: "703920721116",
-    appId: "1:703920721116:web:f5444132ad80346a577819"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
